e5331a1d577e5b47d72a0880c8071a69fc7b811d
Khanh Vu <khanhvu@Khanhs-MacBook-Pro.local>
2018-11-16 14:42:36 -0500

    sum of all multiples of 3 or 5 below 1000

--- alg1-week1.py
+++ alg1-week1.py
@@ -4,24 +4,10 @@
 
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-# sum = 0
-# for i in range(0, 1001):
-#     if i % 3 == 0 or i % 5 == 0:
-#         sum += i
-# print sum
-
-
-fibSeq = [0, 1]
-i = 1
 sum = 0
-x = fibSeq
-while fibSeq[-1] < 4000000:
-    fibSeq.append(fibSeq[i] + fibSeq[i - 1])
-    i += 1
-del fibSeq[-1]
-print fibSeq
-for i in fibSeq[1:len(fibSeq)]:
-    if i % 2 == 0:
-        sum += i 
-print sum 
-    
+for i in range(0, 1001):
+    if i % 3 == 0 or i % 5 == 0:
+        sum += i
+print sum
+
+
